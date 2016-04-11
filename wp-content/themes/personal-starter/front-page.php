0242756577c39b27cdd9abbccb4f340bbc20739b
Cameron Fuller <franksjacket@yahoo.com>
2016-04-11 15:55:51 -0700

    started building out bare bones html and js for website.

---
+++ wp-content/themes/personal-starter/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<p>
+				hello world
+			</p>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
