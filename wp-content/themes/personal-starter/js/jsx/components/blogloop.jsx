3a705461aa9e3d8ad98c965813cdc761198a04d4
Cameron Fuller <franksjacket@yahoo.com>
2016-04-22 18:33:27 -0700

    made blog slide in and reset when last item loads.

--- wp-content/themes/personal-starter/js/jsx/components/blogloop.jsx
+++ wp-content/themes/personal-starter/js/jsx/components/blogloop.jsx
@@ -10,7 +10,8 @@ var BlogLoop = React.createClass({
          postData:[],
          postContent:'',
          postTitle:'',
-         arrayIndex:0
+         arrayIndex:0,
+         activate: false
       };
    },
 
@@ -22,35 +23,45 @@ var BlogLoop = React.createClass({
          success: function(wpData) {
             this.setState({postData:wpData});
          }.bind(this)
-      })
+      });
    },
 
-   testMethod:function() {
-      this.setState({postContent:this.state.postData[this.state.arrayIndex].content.rendered});
-      this.setState({postTitle:this.state.postData[this.state.arrayIndex].title.rendered});
-      if(this.state.arrayIndex ===4){
+   loadBlog:function() {
+
+      this.setState({activate:true});
+      if(this.state.arrayIndex === 4){
          this.setState({arrayIndex:0});
+         this.setState({activate:false})
       } else {
+         this.setState({postContent:this.state.postData[this.state.arrayIndex].content.rendered});
+         this.setState({postTitle:this.state.postData[this.state.arrayIndex].title.rendered});
          this.setState({arrayIndex:this.state.arrayIndex + 1});
-      }
+      };
    },
-
    nextPage: function() {
       browserHistory.push('/test');
    },
    createMarkup: function() {
       return {__html: 'First &middot; Second'};
    },
+   animateBlog: function() {
+      if(this.state.activate===true) {
+         return "animate-blog";
+      }
+   },
    render: function() {
       return (
-         <div>
+         <div className="page-background">
             <p>
                This is generated dynamically through react!
             </p>
-
-            <a onClick={this.testMethod}>Load Blog</a>
-            <h2>{this.state.postTitle}</h2>
-            <div dangerouslySetInnerHTML={{__html: this.state.postContent}}></div>
+            <div className="flex-center-center">
+               <a className="button" onClick={this.loadBlog}>Load Blog</a>
+            </div>
+            <div className={"blog-wrapper flex-center-column container "+this.animateBlog()}>
+               <h2>{this.state.postTitle}</h2>
+               <div dangerouslySetInnerHTML={{__html: this.state.postContent}}></div>
+            </div>
             <p>
                <a onClick={this.nextPage}>Test</a>
             </p>
