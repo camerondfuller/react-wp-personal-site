348d3305d6f83924bae2aeb325b508b9c6061b2c
Cameron Fuller <franksjacket@yahoo.com>
2016-05-15 21:42:35 -0700

    Refactored main to es6.

--- wp-content/themes/personal-starter/js/jsx/main.jsx
+++ wp-content/themes/personal-starter/js/jsx/main.jsx
@@ -1,19 +1,17 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, Redirect } from 'react-router';
 import HomePage from './components/home.jsx';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-
-
-var App = React.createClass({
-   render: function() {
+class App extends React.Component {
+   render () {
       return (
          <Router history={browserHistory}>
             <Route path='/' component={HomePage} />
          </Router>
       );
    }
-});
+}
 
+export default App;
 ReactDOM.render(<App />, document.querySelector('#mountnode'));
